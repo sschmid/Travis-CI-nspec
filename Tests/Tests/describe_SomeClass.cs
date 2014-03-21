e5ed114c2a8e15a367640d7c3e495119929865aa
Simon Schmid <mail@sschmid.com>
2014-03-21 15:37:44 +0100

    Added dependencies

--- Tests/Tests/describe_SomeClass.cs
+++ Tests/Tests/describe_SomeClass.cs
@@ -1,12 +1,13 @@
 ﻿using NSpec;
+using ToolKit;
 
 class describe_SomeClass : nspec {
     void it_passes() {
         new SomeClass().should_not_be_null();
     }
 
-//    void it_fails() {
-//        new SomeClass().should_be_null();
-//    }
+    void it_can_use_dependencies() {
+        new OrderedSet<object>().should_not_be_null();
+    }
 }
 
