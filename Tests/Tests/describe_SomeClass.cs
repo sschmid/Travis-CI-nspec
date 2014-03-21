034ed1c7e5c3c8a59924c2aa214859052843b3dd
Simon Schmid <mail@sschmid.com>
2014-03-21 10:50:31 +0100

    Empty project with nspec

---
+++ Tests/Tests/describe_SomeClass.cs
@@ -0,0 +1,12 @@
+﻿using NSpec;
+
+class describe_SomeClass : nspec {
+    void it_passes() {
+        new SomeClass().should_not_be_null();
+    }
+
+    void it_fails() {
+        new SomeClass().should_be_null();
+    }
+}
+
